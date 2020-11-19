3d1c6a0ed5e79d979ff0326a9cc7a50c5252a1e0
Bruno <bruno@brunos-macbook-pro.local>
2020-11-19 07:37:11 -0800

    Añade 4 archivos

---
+++ ViewController/DetallesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DefensaCD
+//
+//  Created by Bruno on 18/11/2020.
+//  Copyright © 2020 ual. All rights reserved.
+//
+
+import UIKit
+
+class DetallesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
