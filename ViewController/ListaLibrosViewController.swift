8d479eb5040808bfbd8337d5bfbfc9fae959033b
Bruno <bruno@brunos-macbook-pro.local>
2020-11-23 10:03:48 -0800

    Commit mientras el SearchBar funcione

--- ViewController/ListaLibrosViewController.swift
+++ ViewController/ListaLibrosViewController.swift
@@ -13,21 +13,35 @@ class ListaLibroViewController: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var searchBar: UISearchBar!
-    
     @IBOutlet weak var tabBar: UITabBar!
     
+    var searchController: UISearchController!
+    var resultsController = UITableViewController()
+    
     var libros = [Libro]()
+    var filteredBooks = [Libro]()
+    
+    var estadoBuscado: Estado = Estado.seguido
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //searchBar
+            //creamos el SearchBar
+        self.searchController = UISearchController(searchResultsController: self.resultsController)
+        self.tableView.tableHeaderView = self.searchController.searchBar
+        self.searchController.searchResultsUpdater = self
+      
+            //TableSetting
+        self.resultsController.tableView.dataSource = self
+        self.resultsController.tableView.delegate = self
+            //Personalizacion de celda de búsqueda
+        self.resultsController.tableView.rowHeight = 103.5
+        self.resultsController.tableView.estimatedRowHeight = 103.5
         
-        
-        //Tamaño celda
+        //Tamaño celda de búsqueda
         tableView.rowHeight = 103.5
         tableView.estimatedRowHeight = 103.5
-        
+        tableView.backgroundColor = #colorLiteral(red: 0.05098039216, green: 0.05098039216, blue: 0.05098039216, alpha: 1)
         //TabBar
         tabBar.delegate = self
         tabBar.selectedItem = tabBar.items?[0]
@@ -68,17 +82,27 @@ extension ListaLibroViewController: UITableViewDataSource, UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return libros.count
+        if tableView == self.tableView {
+            return libros.count
+        }else{
+            return filteredBooks.count
+        }
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Libro", for: indexPath) as! TableViewCellLibro
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Libro", for: indexPath) as! TableViewCellLibro
         
-        cell.nombre.text = libros[indexPath.row].nombre
-        cell.autor.text = libros[indexPath.row].autor
-        cell.foto.image = libros[indexPath.row].foto
-        //cell.resumen.Autor.text = libros[indexPath.row].autor
+        if tableView == self.tableView {
+            cell.nombre.text = libros[indexPath.row].nombre
+            cell.autor.text = libros[indexPath.row].autor
+            cell.foto.image = libros[indexPath.row].foto
+        }else{
+            cell.nombre.text = filteredBooks[indexPath.row].nombre
+            cell.autor.text = filteredBooks[indexPath.row].autor
+            cell.foto.image = filteredBooks[indexPath.row].foto
+        }
+        
         
         return cell
     }
@@ -89,19 +113,42 @@ extension ListaLibroViewController: UITabBarDelegate {
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
 
         if item.tag == 1 {
-            print("owo1")
-            
+            self.estadoBuscado = Estado.seguido
+            self.resultsController.tableView.reloadData()
         }
         if item.tag == 2 {
-            print("owo2")
+            self.estadoBuscado = Estado.pendiente
+            self.resultsController.tableView.reloadData()
         }
         if item.tag == 3 {
-            print("owo3")
-            
+            self.estadoBuscado = Estado.nada
+            self.resultsController.tableView.reloadData()
         }
         if item.tag == 4 {
             print("owo4")
         }
         print("funciona a medias")
+    }
+}
+
+extension ListaLibroViewController: UISearchResultsUpdating{
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        self.filteredBooks = self.libros.filter{ (libro: Libro) -> Bool in
+            if(libro.nombre.lowercased().contains(self.searchController.searchBar.text!.lowercased())){
+                
+                if estadoBuscado == libro.estado{
+                    return true
+                }else{
+                    return false
+                }
+                
+            } else{
+                return false
+            }
+        }
+        
+        self.resultsController.tableView.reloadData()
     }
 }
