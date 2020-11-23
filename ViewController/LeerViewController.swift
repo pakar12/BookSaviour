d1a92528fa02c083776043e90627ea4b663c5068
Bruno <bruno@brunos-macbook-pro.local>
2020-11-23 07:17:08 -0800

    Listo para cambiar el visor de imagenes

--- ViewController/LeerViewController.swift
+++ ViewController/LeerViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
-class LeerViewController: UIViewController {
+class LeerViewController: UIViewController , UIScrollViewDelegate{
 
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    
+    var imageArray = [UIImage]()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainScrollView.frame = view.frame
+        imageArray = [UIImage(named: "Braco intro 1")!, UIImage(named: "Braco intro 2")!]
+        
+        for i in 0..<imageArray.count{
+            let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            imageView.contentMode = .scaleAspectFit
+            let xPosition = self.view.frame.width * CGFloat(i)
+            
+            imageView.frame = CGRect(x: xPosition, y: 0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+            
+            mainScrollView.contentSize.width = mainScrollView.frame.width * CGFloat(i + 1)
+            mainScrollView.addSubview(imageView)
+        }
+        
+        
+        
+        mainScrollView.minimumZoomScale = 1.0
+        mainScrollView.maximumZoomScale = 6.0
+        
     }
 
     @IBAction func atras(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    func viewForZooming(in ScrollView: UIScrollView)-> UIView? {
+        
+        return mainScrollView.subviews[1]
+    }
 }
 
